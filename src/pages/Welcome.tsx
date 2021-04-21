8f0ca9aaf33095697c377f4fd842e6014e57ccb2
Daniel Miranda <developermiranda@gmail.com>
2021-04-20 22:27:29 -0300

    Code refactoring

--- src/pages/Welcome.tsx
+++ src/pages/Welcome.tsx
@@ -1,17 +1,18 @@
-import React, { useState } from 'react';
-import { SafeAreaView, Text, Image, TouchableOpacity, StyleSheet } from 'react-native';
+import React from 'react';
+import {
+  SafeAreaView,
+  Text,
+  Image,
+  TouchableOpacity,
+  StyleSheet,
+  Dimensions
+} from 'react-native';
 
 import wateringImg from '../assets/watering.png';
-import { Button } from '../components/Button';
 import colors from '../styles/colors';
 
 
 export function Welcome() {
-  const [visible, setVisible] = useState(false);
-
-  function handleVisibility() {
-    setVisible(true);
-  }
 
   return (
     <SafeAreaView style={styles.container}>
@@ -20,16 +21,25 @@ export function Welcome() {
           suas plantas {'\n'}
           de forma fácil
         </Text>
-      {
-        visible &&
-        <Image source={wateringImg} style={styles.image} />
-      }
+      <Image
+        source={wateringImg}
+        style={styles.image}
+        resizeMode="contain"
+      />
+
       <Text style={styles.subtitle}>
         Não esqueça mais de regar suas plantas.
         Nós cuidamos de lembrar você sempre que precisar.
         </Text>
 
-      <Button title=">" />
+      <TouchableOpacity
+        style={styles.button}
+        activeOpacity={0.7}
+      >
+        <Text style={styles.buttonText}>
+          >
+        </Text>
+      </TouchableOpacity>
 
     </SafeAreaView>
   )
@@ -39,7 +49,7 @@ const styles = StyleSheet.create({
   container: {
     flex: 1,
     alignItems: 'center',
-    justifyContent: 'space-between'
+    justifyContent: 'space-around'
   },
   title: {
     fontSize: 32,
@@ -55,7 +65,20 @@ const styles = StyleSheet.create({
     color: colors.heading
   },
   image: {
-    width: 292,
-    height: 284
+    height: Dimensions.get('window').width * 0.7
+  },
+  button: {
+    backgroundColor: colors.green,
+    justifyContent: 'center',
+    alignItems: 'center',
+    borderRadius: 16,
+    marginBottom: 10,
+    height: 56,
+    width: 56,
+    paddingHorizontal: 10
+  },
+  buttonText: {
+    color: colors.white,
+    fontSize: 24
   }
 });
