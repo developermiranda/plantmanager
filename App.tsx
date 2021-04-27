4b1a5f7717c140b3641e92893277b6428c9fb00e
Daniel Miranda <developermiranda@gmail.com>
2021-04-27 00:26:35 -0300

    App concluido

--- App.tsx
+++ App.tsx
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import AppLoading from 'expo-app-loading';
+import * as Notifications from 'expo-notifications';
 
 import Routes from './src/routes';
 import {
@@ -7,12 +8,30 @@ import {
   Jost_400Regular,
   Jost_600SemiBold
 } from '@expo-google-fonts/jost';
+import { PlantProps } from './src/libs/storage';
 
 export default function App() {
   const [fontsLoaded] = useFonts({
     Jost_400Regular,
     Jost_600SemiBold
   });
+
+  useEffect(() => {
+    const subscription = Notifications.addNotificationReceivedListener(
+      async notifications => {
+        const data = notifications.request.content.data.plant as PlantProps;
+        console.log(data);
+      });
+
+    return () => subscription.remove();
+
+    // async function notifications() {
+    //   const data = await Notifications.cancelAllScheduledNotificationsAsync();
+    //   console.log(`###### NOTIFICAÇÕES AGENDADAS ######`)
+    //   console.log(data);
+    // }
+    // notifications();
+  }, [])
 
   if (!fontsLoaded)
     return <AppLoading />
